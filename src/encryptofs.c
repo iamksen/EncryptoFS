cab9225bd86ad7bd0b4edbe4d79deb2452521c3c
Roshan Jha <roshjha012@gmail.com>
2017-11-08 23:06:59 +0530

    Incorporated Encryption to file contents

--- src/encryptofs.c
+++ src/encryptofs.c
@@ -9,12 +9,89 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
 
 typedef struct {
 	char *rootdir;
 	char *key;
 	char keys[32];
 } en_state;
+
+
+char* buf_crypt(const char* inbuf, int inlen, int* outlen, int action, char* key_str){
+    	int tmplen;
+    	unsigned char* tmpbuf = (unsigned char*) malloc(inlen + EVP_MAX_BLOCK_LENGTH);
+   	char* outbuf = (char*) malloc(inlen + EVP_MAX_BLOCK_LENGTH);
+
+    	/* OpenSSL libcrypto vars */
+    	EVP_CIPHER_CTX ctx;
+    	unsigned char key[32];
+    	unsigned char iv[32];
+    	int nrounds = 5;
+    
+    	/* tmp vars */
+    	int i;
+
+   	/* Setup Encryption Key and Cipher Engine */
+	if(!key_str){
+	    /* Error */
+	    fprintf(stderr, "Key_str must not be NULL\n");
+	    return 0;
+	}
+	
+	/* Build Key from String */
+	i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), NULL,
+			   (unsigned char*)key_str, strlen(key_str), nrounds, key, iv);
+	if (i != 32) {
+	    /* Error */
+	    fprintf(stderr, "Key size is %d bits - should be 256 bits\n", i*8);
+	    return 0;
+	}
+	
+	/* Init Engine */
+	EVP_CIPHER_CTX_init(&ctx);
+	//Action 1 = encrypt, 0 = decrypt
+	EVP_CipherInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv, action);   
+	
+	
+	/* perform cipher transform on block */
+    	if(!EVP_CipherUpdate(&ctx, tmpbuf, &tmplen, (const unsigned char*) inbuf, inlen)){
+	    /* Error */
+	    EVP_CIPHER_CTX_cleanup(&ctx);
+	    return 0;
+	}
+	
+	
+	/* Write Block */
+	memcpy(outbuf, tmpbuf, tmplen);
+	*outlen = tmplen;
+
+	/* Handle remaining cipher block + padding */
+	if(!EVP_CipherFinal_ex(&ctx, tmpbuf, &tmplen)){
+		/* Error */
+		EVP_CIPHER_CTX_cleanup(&ctx);
+		return 0;
+	}
+	/* Write remainign cipher block + padding*/
+	memcpy(outbuf + *outlen, tmpbuf, tmplen);
+	*outlen += tmplen;
+
+	
+	//EVP_CIPHER_CTX_cleanup(&ctx);
+	
+    	/* Success */
+    	return outbuf;
+}
 
 void encrypt(char *epath, const char *path)
 {
@@ -87,7 +164,6 @@ int en_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t off
 			strcpy(dname, de->d_name);
 		else
 			decrypt(dname, de->d_name);
-		
 		st.st_ino = de->d_ino;
 		if( filler(buffer, dname, &st, telldir(dp)) )
 			break;
@@ -140,15 +216,46 @@ int en_rmdir(const char *path)
 
 int en_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi)
 {
+	
 	char fpath[PATH_MAX];
 	fullpath(fpath, path);
-	int fd = open(fpath, O_RDONLY);
-	return read(fd, buffer, size);
+
+	//int fd = open(fpath, O_RDONLY);
+	//return read(fd, buffer, size);
+	//en_state *state = (en_state *)(fuse_get_context()->private_data);
+	//strcpy(fpath, state->rootdir);
+	//strncat(fpath, path, PATH_MAX); 
+
+	
+	char etext[PATH_MAX];
+	FILE *fp = fopen(fpath, "r");
+	fscanf(fp, "%s", etext);
+	fclose(fp);
+	
+	int len;
+
+	char *dtext = buf_crypt(etext, strlen(etext), &len, 0, "abc");
+	memcpy(buffer, dtext + offset, size);
+	return strlen(buffer) - offset;
+
 }
 
 int en_write(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-	return pwrite(fi->fh, buffer, size, offset);
+	char fpath[PATH_MAX];
+	fullpath(fpath, path);
+	
+	int len; 
+
+	char *encrypted_text = buf_crypt(buffer, strlen(buffer), &len, 1, "abc");
+	
+	/* Write Encrypted text to File */
+
+	FILE *fp = fopen(fpath, "w");
+	fprintf(fp, "%s", encrypted_text);
+	fclose(fp);
+
+	return 1;
 }
 
 int en_unlink(const char *path)
